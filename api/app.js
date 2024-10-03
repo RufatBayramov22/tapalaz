df239019399c8c523997e8431d460a6d5089e4fb
RufatBayramov22 <ibrahimovrufka@icloud.com>
2024-10-03 22:51:28 +0400

    new apps

--- api/app.js
+++ api/app.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express from "express-serve-static-core"
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
