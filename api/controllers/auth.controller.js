2f10e3dedb5b00b6a7eb2b83c87087d9d0efae37
RufatBayramov22 <ibrahimovrufka@icloud.com>
2024-10-05 14:40:31 +0400

    new

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -28,58 +28,49 @@ export const register = async (req, res) => {
   }
 };
 
-
-
+// LOGIN FUNCTION
 export const login = async (req, res) => {
   const { username, password } = req.body;
 
   try {
     // CHECK IF THE USER EXISTS
-
-    const user = await User.findOne({
-      where: { username },
-    });
+    const user = await User.findOne({ username });
 
     if (!user) return res.status(400).json({ message: "Invalid Credentials!" });
 
     // CHECK IF THE PASSWORD IS CORRECT
-
     const isPasswordValid = await bcrypt.compare(password, user.password);
 
     if (!isPasswordValid)
       return res.status(400).json({ message: "Invalid Credentials!" });
 
+    // Token'ı oluşturmak için gerekli süreyi tanımlayın
+    const age = 1000 * 60 * 60 * 24 * 7; // 7 gün
+
     // GENERATE COOKIE TOKEN AND SEND TO THE USER
-
-    // res.setHeader("Set-Cookie", "test=" + "myValue").json("success")
-    const age = 1000 * 60 * 60 * 24 * 7;
-
     const token = jwt.sign(
       {
-        id: user.id,
+        id: user._id,
         isAdmin: false,
       },
-      process.env.JWT_SECRET_KEY,
+      "my-secret-key",
       { expiresIn: age }
     );
 
-    const { password: userPassword, ...userInfo } = user;
+    const { password: userPassword, ...userInfo } = user.toObject();
 
-    res
-      .cookie("token", token, {
-        httpOnly: true,
-        // secure:true,
-        maxAge: age,
-      })
-      .status(200)
-      .json(userInfo);
+    res.cookie("token", token, {
+      httpOnly: true,
+      // secure: true, // Eğer HTTPS kullanıyorsanız bu satırı açın
+      maxAge: age,
+    })
+    .status(200)
+    .json({ token, ...userInfo }); // Token'ı yanıtın içine ekliyoruz
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to login!" });
   }
 };
-
-
 // LOGOUT FUNCTION
 export const logout = (req, res) => {
   res.clearCookie("token").status(200).json({ message: "Logout Successful" });
