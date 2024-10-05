71edaa4caed6889e63536040ee89eb328f9adc5d
RufatBayramov22 <ibrahimovrufka@icloud.com>
2024-10-05 16:18:58 +0400

    new

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -19,13 +19,12 @@ export const register = async (req, res) => {
 
     await newUser.save();
 
-    // GENERATE A TOKEN
-    const token = jwt.sign({ id: newUser._id }, JWT_SECRET_KEY, { expiresIn: '1h' });
-
-    res.status(201).json({ message: "User created successfully", token });
+    res.status(201).json({ message: "User created successfully" });
   } catch (err) {
-    console.error("Error details:", err);
-    res.status(500).json({ message: "Failed to create user!", error: err.message });
+    console.error("Error details:", err); // Daha fazla bilgi için
+    res
+      .status(500)
+      .json({ message: "Failed to create user!", error: err.message });
   }
 };
 
