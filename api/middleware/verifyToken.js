32073993e31b0d68feafbb25215bdc50b97125f6
RufatBayramov22 <ibrahimovrufka@icloud.com>
2024-10-05 13:53:35 +0400

    new

--- api/middleware/verifyToken.js
+++ api/middleware/verifyToken.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 // Yetkilendirme Middleware
 export const authMiddleware = (req, res, next) => {
   const token = req.cookies.token; // Token'ı cookie'den al
-
+  console.log(req.cookies.token)
   
   if (!token) {
     return res.status(401).json({ message: 'No token provided, authorization denied' });
--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,4 +1,6 @@
 import { createContext, useEffect, useState } from "react";
+import { jwtDecode } from "jwt-decode"; // Düzgün içe aktarma
+import { useNavigate } from "react-router-dom";
 
 export const AuthContext = createContext();
 
@@ -7,18 +9,36 @@ export const AuthContextProvider = ({ children }) => {
     JSON.parse(localStorage.getItem("user")) || null
   );
 
-  const [token, setToken] = useState(localStorage.getItem("token") || null); // Token state'i ekleyin
-
+  const [token, setToken] = useState(localStorage.getItem("token") || null);
   const updateUser = (data) => {
     setCurrentUser(data);
   };
 
-  useEffect(() => {
-    localStorage.setItem("user", JSON.stringify(currentUser));
-  }, [currentUser]);
+
+  const isTokenExpired = (token) => {
+    if (!token) return true; // Eğer token yoksa süresi dolmuş kabul et
+    const decoded = jwtDecode(token); // jwt_decode yerine jwtDecode kullan
+    const currentTime = Date.now() / 1000; // Şu anki zamanı saniye cinsinden al
+    return decoded.exp < currentTime; // Eğer süresi dolmuşsa true döndür
+  };
 
   useEffect(() => {
-    localStorage.setItem("token", token); // Token'ı yerel depolamaya kaydedin
+    const checkTokenExpiry = () => {
+      if (isTokenExpired(token)) {
+        updateUser(null); // Kullanıcı bilgisini sıfırlayın
+        setToken(null); // Token'ı sıfırlayın
+        localStorage.removeItem("token"); // Yerel depolamadan token'ı kaldırın
+    
+      }
+    };
+
+    checkTokenExpiry(); // Token süresini kontrol et
+
+    // Belirli aralıklarla kontrol etmek için bir interval ayarlayın (örneğin, her 5 dakikada bir)
+    const interval = setInterval(checkTokenExpiry, 7 * 24 * 60 * 60 * 1000); // 1 haftada bir kontrol et
+
+
+    return () => clearInterval(interval); // Temizle
   }, [token]);
 
   return (
